b11eefda96ba5c6fc79b7d9d5f7af8da95e75ade
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-12-30 16:26:28 -0500

    Done.

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,5 +1,34 @@
+require 'pry'
 def fibonacci(num)
   # type your code in here
+  # fib = []
+  # num.times do
+  #   if fib.length == 0
+  #     fib << 0
+  #   elsif fib.length == 1
+  #     fib << 1
+  #   else
+  #     # binding.pry
+  #     fib << fib.last + fib[fib.length - 2]
+  #   end
+  # end
+  # fib.length >= 2 ? fib.last + fib[fib.length - 2] : fib.last
+
+  last = [0, 1]
+
+  sum = 0
+
+  if num < 2
+    last[num]
+  else
+    (num - 1).times do
+      sum = last.sum
+      last = [last[1], sum]
+    end
+    last.last
+  end
+  
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -17,7 +46,11 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", fibonacci(10)
 
   # Don't forget to add your own!
+
+  puts "Expecting: "
 end
+
+# binding.pry
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
